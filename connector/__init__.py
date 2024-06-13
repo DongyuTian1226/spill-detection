9d20da89d51f27a031fae9b1aae852c3b208ce5b
Dongyu Tian <2502533535@qq.com>
2024-06-13 19:25:49 +0800

    feat: add log in the http

--- connector/__init__.py
+++ connector/__init__.py
@@ -1,5 +1,7 @@
+import os
 import requests
-
+import logging
+from datetime import datetime
 
 '''Connect the server and algorithms by Kafka.'''
 
@@ -15,7 +17,7 @@ class HttpPoster():
     poster.run(data)
     将data上报到url
     '''
-    def __init__(self, url):
+    def __init__(self, url, deviceID: str = None, deviceType: int = 0, level=logging.DEBUG):
         '''function __init__
 
         input
@@ -23,6 +25,33 @@ class HttpPoster():
         url: str, http上报的url
         '''
         self.url = url
+        if deviceID is not None:
+            self._initHttpLogger(deviceID, deviceType, level)
+
+    def _initHttpLogger(self, deviceID: str, deviceType: int = 200, level=logging.DEBUG):
+        self.logger = logging.getLogger(deviceID + '_' + str(deviceType))
+        self.logger.setLevel(level)
+        fmtStrList = ['asctime', 'name', 'levelname', 'filename', 'message']
+        fmtStr = ' - '.join(['%('+i+')s' for i in fmtStrList])
+        formatter = logging.Formatter(fmtStr)
+        # 控制台日志
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        ch.setLevel(logging.ERROR)
+        self.logger.addHandler(ch)
+        # 文件日志
+        logsDir = './logger/logs'
+        if not os.path.exists(logsDir):     # 判断两层文件夹是否存在
+            os.mkdir(logsDir)
+        deviceDir = f'{logsDir}/{self.logger.name}'
+        if not os.path.exists(deviceDir):
+            os.mkdir(deviceDir)
+        filePath = f'./logger/logs/{deviceID + '_' + str(deviceType)}/' + \
+            f'http-{datetime.now().strftime("%Y-%m-%d")}.log'
+        fh = logging.FileHandler(filePath, encoding='utf-8')
+        fh.setFormatter(formatter)
+        fh.setLevel(level)
+        self.logger.addHandler(fh)
 
     def run(self, events: list):
         '''function run
@@ -47,5 +76,14 @@ class HttpPoster():
 
         将字典格式的数据以POST形式上传给http
         '''
-        r = requests.post(self.url, data=data)
+        # r = requests.post(self.url, data=data)
+        r = requests.post(self.url, json=data)
+        self.logger.debug(f"Post data: {data}")
+        if r.status_code // 100 != 2:
+            print(f"Failed to upload data, status code: {r.status_code}")
+            print("Response body:")
+            try:
+                print(r.json())  # Try to parse the response body as JSON
+            except ValueError:
+                print(r.text)  # If the response body is not JSON, print it as is
         return r
